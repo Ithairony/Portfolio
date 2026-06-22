716f87ed89f62b7e0b14155595f1410fcc8f6705
Ithairony <ithairony21@gmail.com>
2026-06-22 21:05:43 +0100

    Decided to make a single page web App, now almost everything is ready, I just need to edit some content and link other repos

--- Script/script.js
+++ Script/script.js
@@ -30,6 +30,43 @@ document.addEventListener("DOMContentLoaded", function () {
     let isDeleting = false;
     let typingDelay = 100;
 
+    // Buttom back to top behaviour 
+    const backToTopBtn = document.getElementById("backToTop");
+
+    // Show when scrolling down
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            backToTopBtn.classList.add("show");
+        } else {
+            backToTopBtn.classList.remove("show");
+        }
+    });
+
+    // Making it smooth 
+    backToTopBtn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth"});
+    });
+
+    // Getting gitHub repositories languages
+    document.querySelectorAll('.project-card[data-repo]').forEach(card => {
+        const repo = card.dataset.repo;
+        const langEl = card.querySelector('.repo-language');
+        if (!langEl) return;
+
+    fetch(`https://api.github.com/repos/${repo}`)
+        .then(res => {
+            if (!res.ok) throw new Error('GitHub API error: ' + res.status);
+            return res.json();
+        })
+        .then(data => {
+            langEl.textContent = data.language || 'N/A';
+        })
+        .catch(err => {
+            console.error('Failed to load repo info for', repo, err);
+            langEl.textContent = 'N/A';
+        });
+    });
+
     function type() {
         const currentText = texts[textIndex];
         const typingElement = document.querySelector(".typing-text");
